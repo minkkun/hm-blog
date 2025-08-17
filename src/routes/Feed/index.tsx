62b64cd08fdc89c3f85e2a492ae4bf7f403f5b4f
minkkun <minkdono@gmail.com>
2025-08-17 21:09:35 +0700

    add music

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -49,6 +49,15 @@ const Feed: React.FC<Props> = () => {
         }}
       >
         <ProfileCard />
+        <iframe
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/track/2ch4KLYvi9ZGq2pNyEsMom?utm_source=generator"
+        width="100%"
+        height="152"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
         <ContactCard />
         <div className="footer">
           <Footer />
