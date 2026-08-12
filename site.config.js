62b11f0117db1503a7a530174dd65bc1f332a9c3
minkkun <minkdono@gmail.com>
2026-08-13 01:57:16 +0700

    update pfp

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hien minh",
-    image: "/redball.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/beanbagbeach.JPG", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "quirky events in my mind",
     email: "minhhienle.w@gmail.com",
